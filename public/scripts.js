831988d1dc202e3693d644e6dfff37b561f8ba98
Dgeison <paradg@gmail.com>
2021-02-12 12:34:53 -0300

    ajustes finais

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,4 @@
+const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".cards");
 
 for (let card of cards) {
@@ -7,12 +8,5 @@ for (let card of cards) {
   });
 }
 
-// const modalOverlay = document.querySelector(".modal-overlay");
-// const cards = document.querySelectorAll(".card");
 
-// for (let card of cards) {
-//   card.addEventListener("click", function () {
-//     const videoId = card.getAttribute("id");
-//     window.location.href = `/video?id=${videoId}`;
-//   });
-// }
+
